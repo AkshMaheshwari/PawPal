0c489d2fab688c750247dfdde5734c58acdb0814
AkshMaheshwari <aksh.maheshwari05@gmail.com>
2025-10-24 11:21:48 +0530

    update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,6 +7,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  publicDir: 'public',  // Add this line
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
